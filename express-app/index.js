02b29a0eec7da1ea2b5ff6230de32e2459208dea
sonymuhamad <sonyfadhil11@gmail.com>
2025-04-07 19:27:37 +0700

    add graceful shutdown

--- express-app/index.js
+++ express-app/index.js
@@ -1,8 +1,27 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const http = require('http');
 
 const app = express()
 const port = 3000
+
+const users = [
+    {
+        id:5,
+        name:"sony",
+        age:99
+    },
+    {
+        id:10,
+        name:"muhamad",
+        age:100
+    },
+    {
+        id:15,
+        name:"fadhil",
+        age:990
+    }
+]
 
 app.use(bodyParser.json())
 
@@ -10,6 +29,34 @@ app.get("/",(req,res)=>{
     res.send("hello world")
 })
 
-app.listen(port,()=>{
-    console.log(`running express server on port ${port}`)
+app.get("/users",(req,res)=>{
+    res.send(users)
 })
+
+// Create server
+const server = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Running Express server on port ${PORT}`);
+});
+
+
+// Graceful shutdown
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
+
+function shutdown() {
+  console.log('Shutting down gracefully...');
+  server.close(() => {
+    console.log('Closed out remaining connections.');
+    process.exit(0);
+  });
+
+  // Force shutdown if not closed in 10s
+  setTimeout(() => {
+    console.error('Forcing shutdown.');
+    process.exit(1);
+  }, 10000);
+}
