782f7fe287d9418257c023cc30135266d2c2b07d
sonymuhamad <sonyfadhil11@gmail.com>
2025-04-15 10:45:41 +0700

    test shell script

---
+++ key-value-app/start-db.sh
@@ -0,0 +1,12 @@
+MONGODB_IMAGE="mongodb/mongodb-community-server"
+MONGODB_TAG="7.0-ubuntu2204"
+CONTAINER_NAME="mongodb"
+
+# ROOT CREDENTIALS
+ROOT_USER="root-user"
+ROOT_PASSWORD="root-password"
+
+docker run -rm -d --name $CONTAINER_NAME \
+    -e MONGODB_INITDB_ROOT_USERNAME=$ROOT_USER \
+    -e MONGODB_INITDB_ROOT_PASSWORD=$ROOT_PASSWORD \
+    $MONGODB_IMAGE:$MONGODB_TAG
